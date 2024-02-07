6a5a43da1e7b3e614fecf1b455b9c9d69e0aa368
hugolnx <hugolnx@gmail.com>
2024-02-07 14:24:12 -0300

    Adds sass and bulma

--- pages/index/+Page.vue
+++ pages/index/+Page.vue
@@ -10,3 +10,16 @@
 <script setup>
 import Counter from './Counter.vue';
 </script>
+
+<style lang="scss">
+$back-color: #fcc;
+$font-color: #88c;
+
+body {
+  background-color: $back-color;
+
+  h1 {
+    color: $font-color;
+  }
+}
+</style>
