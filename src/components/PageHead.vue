01f88ccc3e96910e1a9ca460940b2af41986eea9
hugolnx <hugolnx@gmail.com>
2024-03-20 20:11:15 -0300

    Removes metatags if it has no content

--- src/components/PageHead.vue
+++ src/components/PageHead.vue
@@ -25,7 +25,7 @@ const {
     socialImage,
 } = data;
 
-const metatags = {
+const rawMetatags = {
     viewport: 'width=device-width, initial-scale=1',
     'og:title': title,
     'og:description': description,
@@ -35,4 +35,8 @@ const metatags = {
     'og:image': socialImage,
     'google-site-verification': import.meta.env.VITE_GOOGLE_SITE_VERIFICATION,
 };
+
+const metatags = Object.fromEntries(
+    Object.entries(rawMetatags).filter(([, content]) => content),
+);
 </script>
